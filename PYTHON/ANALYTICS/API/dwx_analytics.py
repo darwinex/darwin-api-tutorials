10ae59a731bf4719058ee378b1437ad728570024
Ali Saif <33300041+integracore2@users.noreply.github.com>
2019-10-17 13:19:32 +0200

    Added get_quotes_from_ftp() function
    Download Quote data for any DARWIN directly via FTP.
                    
    Parameters
    ----------
    darwin : str
    	DARWIN ticker symbol, e.g. $PLF
    	
    suffix : str
    	Reflects risk, '4.1' being for 10% VaR DARWIN assets.
    	
    monthly : bool
    	If set to True, month and year arguments are ignored, and ALL 
    	data available for the DARWIN is downloaded.
    	
    month : str
    	Data on the FTP server has the following directory structure:    
    		DARWIN_Symbol -> {year}-{month} -> *.csv.gz files
    		e.g. PLF/2019-01/PLF....csv.gz
    		
    		Specifies month for {year}-{month} tuple as above.
    
    year : str
    	Data on the FTP server has the following directory structure:    
    		DARWIN_Symbol -> {year}-{month} -> *.csv.gz files
    		e.g. PLF/2019-01/PLF....csv.gz
    		
    		Specifies year for {year}-{month} tuple as above.
    	
    Returns
    -------
    df
    	Pandas DataFrame containing Quotes, indexed by timeframe.
    --

--- PYTHON/ANALYTICS/API/dwx_analytics.py
+++ PYTHON/ANALYTICS/API/dwx_analytics.py
@@ -4,7 +4,7 @@
     --
     @author: Darwinex Labs (www.darwinex.com)
     
-    Last Updated: September 09, 2019
+    Last Updated: October 17, 2019
     
     Copyright (c) 2017-2019, Darwinex. All rights reserved.
     
@@ -14,20 +14,16 @@
     You may obtain a copy of the License at:    
     https://opensource.org/licenses/BSD-3-Clause
 """
-import os
+import gzip
 import pandas as pd
+from tqdm import tqdm
 from ftplib import FTP
 from io import BytesIO
 from matplotlib import pyplot as plt
 
-# Change to project root directory (optional)
-# os.chdir('<INSERT-PATH-TO-PROJECT-DIR-HERE>')
-
-from MINIONS.dwx_file_io import load_config
-
 class dwx_data_analytics():
     
-    def __init__(self, config='CONFIG/creds_analytics_ftp.cfg'):
+    def __init__(self, config='<ENTER-FTP-CREDENTIALS-CFG-FILE-PATH-HERE>'):
         
         """Initialize variables, setup byte buffer and FTP connection.
         
@@ -61,6 +57,11 @@ class dwx_data_analytics():
         # Setup data access mode (file or FTP)
         self.mode = 0 # Default is file.
         
+        def load_config(_filename=''):
+            return {_key: _value for _key, _value\
+                    in (l.replace('\n','').split('=')\
+                        for l in open(_filename))}
+
         # FTP credentials
         self.ftp_credentials = load_config(config)
         
@@ -284,7 +285,7 @@ class dwx_data_analytics():
                 
         # Return processed data
         return df.dropna()
-        
+    
     ##########################################################################
     
     def get_return_divergence(self, darwin, plot=True):
@@ -410,5 +411,120 @@ class dwx_data_analytics():
                     figsize=(10,8))
             
         return df
+    
+    ##########################################################################
+    
+    def get_quotes_from_ftp(self, 
+                            darwin='PLF',
+                            suffix='4.1',
+                            monthly=True, # If set to False, month/year used.
+                            month='01',
+                            year='2019'):
+        
+        """Download Quote data for any DARWIN directly via FTP.
+                
+        Parameters
+        ----------
+        darwin : str
+            DARWIN ticker symbol, e.g. $PLF
+            
+        suffix : str
+            Reflects risk, '4.1' being for 10% VaR DARWIN assets.
+            
+        monthly : bool
+            If set to True, month and year arguments are ignored, and ALL 
+            data available for the DARWIN is downloaded.
+            
+        month : str
+            Data on the FTP server has the following directory structure:    
+                DARWIN_Symbol -> {year}-{month} -> *.csv.gz files
+                e.g. PLF/2019-01/PLF....csv.gz
+                
+                Specifies month for {year}-{month} tuple as above.
+        
+        year : str
+            Data on the FTP server has the following directory structure:    
+                DARWIN_Symbol -> {year}-{month} -> *.csv.gz files
+                e.g. PLF/2019-01/PLF....csv.gz
+                
+                Specifies year for {year}-{month} tuple as above.
+            
+        Returns
+        -------
+        df
+            Pandas DataFrame containing Quotes, indexed by timeframe.
+        --
+        """
+        quote_files = []
+        roots = []
+        
+        if monthly:
+            
+            tqdm.write(f'\n[KERNEL] Searching for Quote data, please wait..', end='')
+            self.server.retrlines(f'NLST {darwin}/quotes/', roots.append)
+            
+            roots_pbar = tqdm(roots, position=0, leave=True)
+            for root in roots_pbar:
+                try:
+                    roots_pbar.set_description("Getting filenames for month: %s" % root)
+                    root_files = []
+                
+                    self.server.retrlines(f'NLST {darwin}/quotes/{root}', root_files.append)
+                
+                    # Finalize filenames
+                    quote_files += [f'{darwin}/quotes/{root}/{root_file}'\
+                                    for root_file in root_files if suffix in root_file]
+                except Exception as ex:
+                    print(ex)
+                    return
+            
+        elif pd.to_numeric(month) > 0 and pd.to_numeric(year) > 2010:
+            
+            tqdm.write(f'\n[KERNEL] Asserting data for month {year}-{month}, please wait..', end='')    
+            quote_files = []
+            
+            try:
+                self.server.retrlines(f'NLST {darwin}/quotes/{year}-{month}/', quote_files.append)
+                quote_files = [f'{darwin}/quotes/{year}-{month}/{quote_file}'\
+                                    for quote_file in quote_files if suffix in quote_file]
+            except Exception as ex:
+                print(ex)
+                return
+        else:
+            print('\n[ERROR] Please either set monthly=True or ensure both month and year have integer values')
+            return
+            
+        # Process tick data files
+        tqdm.write(f'\n[KERNEL] {len(quote_files)} files retrieved.. post-processing now, please wait..', end='')
+        ticks_df = pd.DataFrame()
+        ticks_pbar = tqdm(quote_files, position=0, leave=True)
+        
+        for tick_file in ticks_pbar:
+            
+            try:
+                ticks_pbar.set_description("Processing %s" % tick_file)
+                
+                # Clear / reinitialize buffer
+                self.retbuf = BytesIO()
+            
+                self.server.retrbinary(f"RETR {tick_file}", self.retbuf.write)
+                self.retbuf.seek(0)
+                
+                # Extract data from BytesIO object
+                ret = [line.strip().decode().split(',') for line in gzip.open(self.retbuf)]
+                ticks_df = pd.concat([ticks_df, pd.DataFrame(ret[1:])], axis=0)
+                
+            except Exception as ex:
+                print(ex)
+        
+        # Clean up
+        ticks_df.columns = ['timestamp','quote']
+        ticks_df.timestamp = ticks_df.timestamp.apply(pd.to_numeric)
+        ticks_df.set_index('timestamp', drop=True, inplace=True)
+        ticks_df.index = pd.to_datetime(ticks_df.index, unit='ms')
+        ticks_df.quote = ticks_df.quote.apply(pd.to_numeric)
+        
+        # Return DataFrame
+        return ticks_df.dropna()
     
     ##########################################################################
